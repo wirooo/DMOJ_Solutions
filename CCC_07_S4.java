2573f9e2188a50d5d88598634c37a089c50be4e4
wirooo <willh.lu@gmail.com>
2017-11-22 13:48:48 -0500

    Start CCC_07_S4 Waterpark

---
+++ CCC_07_S4.java
@@ -0,0 +1,13 @@
+package DMOJ_Solutions;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CCC_07_S4 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(stdin.readLine());
+
+    }
+}
